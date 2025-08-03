070204677ce4218de782f2596687b36103a315ed
sadrw-art <inezaemile8@gmail.com>
2025-08-03 13:20:04 +0200

    Fix syntax error in app.py

--- app.py
+++ app.py
@@ -1,12 +1,6 @@
-from flask import Flask
-import random
+from flask import Flask app = Flask(__name__) 
+@app.route('/') def home():
+    return "✅ Aviator Predictor is Running!" if __name__ 
+== '__main__':
+    app.run()
 
-app = Flask(__name__)
-
-@app.route("/")
-def home():
-    prediction = round(random.uniform(1.00, 20.00), 2)
-    return f"<h1 style='text-align:center;'>🚀 Aviator Predictor: <span style='color:red;'>{prediction}x</span></h1>"
-
-if __name__ == "__main__":
-    app.run(debug=True)
